2928be3d3e50c98722c1b846161534519f4631f9
Rob Hailman <robhailman@gmail.com>
2015-12-11 23:52:12 -0500

    Implement aspect ratio correction
    Jiggery-pokery with render texture and two cameras to allow world space
    to be in 320x200, but display with a proper square-pixel ratio.
    
    Also placeholder plane sprite and player controller if we're doing the
    cropduster thing.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+
+    public GUITexture RenderTexture;
+
+    private Rect screenRect;
+    private float pixelRatioAdjustment = 1.6f;  // aspect ratio of a CGA screen
+
+    // Use this for initialization
+    void Start()
+    {
+        screenRect = new Rect(0, 0, Screen.width, Screen.height);
+        RenderTexture.pixelInset = screenRect;
+        float aspectRatio = screenRect.width / screenRect.height;
+        RenderTexture.transform.localScale = new Vector3(1, aspectRatio * pixelRatioAdjustment);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Screen.width != screenRect.width || Screen.height != screenRect.height)
+        {
+            screenRect = new Rect(0, 0, Screen.width, Screen.height);
+            RenderTexture.pixelInset = screenRect;
+            float aspectRatio = screenRect.width / screenRect.height;
+            RenderTexture.transform.localScale = new Vector3(1, aspectRatio * pixelRatioAdjustment);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float rotationSpeed = 5;
+    public float rotation = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rotation -= Input.GetAxis("Vertical") * rotationSpeed;
+
+        if (rotation < 0)
+        {
+            rotation += 360;
+        }
+        else if (rotation > 360)
+        {
+            rotation -= 360;
+        }
+        Quaternion target = Quaternion.Euler(0, 0, rotation);
+        transform.rotation = target;
+    }
+}
