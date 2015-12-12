407bc8aec7151d79854e357cdabdce1f12c8544b
Rob Hailman <robhailman@gmail.com>
2015-12-12 12:48:38 -0500

    Add flight dynamics
    These don't feel great

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,64 +5,48 @@ public class PlayerController : MonoBehaviour
 {
 
     public float RotationSpeed = 5;
-    public float FlightSpeed = 1;
+    public float FlightSpeed = 3;
+
+    public float StallSpeed = 1;
+    public float OutofStallSpeed = 2.5f;
+    public float CriticalAngle = 45;
+    public float AdjustSpeed = 0.1f;
+    public float StallRotation = 1f;
+
+    public float LevelFlightLerpT = 0.25f;
 
     public Sprite[] planeSprites;
 
+    private float currentSpeed;
     private float rotation = 0;
     private Transform sprayArea;
+    private bool isStalled;
+
+    #region MonoBehaviours
 
     // Use this for initialization
     void Start()
     {
         sprayArea = GameObject.Find("SprayArea").transform;
+        currentSpeed = FlightSpeed;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
 
-        // calculate rotation
-        rotation -= Input.GetAxis("Vertical") * RotationSpeed;
-
-        if (rotation < 0)
-        {
-            rotation += 360;
-        }
-        else if (rotation > 360)
-        {
-            rotation -= 360;
-        }
-
-        // update sprite based on rotation
-        int spriteIndex = Mathf.FloorToInt((rotation + 22) / 45);
-        if (spriteIndex >= planeSprites.Length)
-        {
-            spriteIndex = 0;
-        }
-        this.GetComponent<SpriteRenderer>().sprite = planeSprites[spriteIndex];
-
-        // rotate spray area
-        Quaternion target = Quaternion.Euler(0, 0, rotation);
-        sprayArea.rotation = target;
-
-        // disable spray area if not flying level
-
-        if (22f < rotation && rotation < 338f)
-        {
-            sprayArea.GetComponent<Collider2D>().enabled = false;
-        }
-        else
-        {
-            sprayArea.GetComponent<Collider2D>().enabled = true;
-        }
+        ProcessInput();
+        FlightDynamics();
+        UpdateSprite();
+        HandleSprayer();
 
         // fly plane
-        float xSpeed = Mathf.RoundToInt(Mathf.Cos(rotation * Mathf.Deg2Rad) * FlightSpeed);
-        float ySpeed = Mathf.RoundToInt(Mathf.Sin(rotation * Mathf.Deg2Rad) * FlightSpeed);
+        float xSpeed = Mathf.RoundToInt(Mathf.Cos(rotation * Mathf.Deg2Rad) * currentSpeed);
+        float ySpeed = Mathf.RoundToInt(Mathf.Sin(rotation * Mathf.Deg2Rad) * currentSpeed);
 
         transform.position += new Vector3(xSpeed, ySpeed);
     }
+
 
     public void OnCollisionStay2D(Collision2D collision)
     {
@@ -78,4 +62,99 @@ public class PlayerController : MonoBehaviour
             Debug.Log("You lose!");
         }
     }
+
+    #endregion MonoBehaviours
+
+    #region Private methods
+
+    private void FlightDynamics()
+    {
+        if (rotation > CriticalAngle && rotation < (180 - CriticalAngle))
+        {
+            // if above critical angle up, reduce speed
+            currentSpeed -= AdjustSpeed;
+            Debug.Log("Flying up " + currentSpeed);
+        }
+
+        else if (rotation > (180 + CriticalAngle) && rotation < (360 - CriticalAngle))
+        {
+            // if below critical angle down, increase speed
+            currentSpeed += AdjustSpeed;
+            Debug.Log("Flying down " + currentSpeed);
+        }
+
+        else
+        {
+            // if flying level, lerp back to FlightSpeed
+            currentSpeed = Mathf.Lerp(currentSpeed, FlightSpeed, LevelFlightLerpT);
+            Debug.Log("Level " + currentSpeed);
+        }
+
+        if (currentSpeed <= StallSpeed && !isStalled)
+        {
+            isStalled = true;
+            Debug.Log("Stalled!");
+        }
+
+        if (isStalled)
+        {
+            if (rotation < 90 || rotation > 270)
+            {
+                rotation -= StallRotation;
+                if (rotation < 0) rotation += 360;
+            }
+            else if (rotation > 90 || rotation < 270)
+            {
+                rotation += StallRotation;
+            }
+            if (currentSpeed >= OutofStallSpeed)
+            {
+                isStalled = false;
+                Debug.Log("Out of stall!");
+            }
+        }
+    }
+
+    private void HandleSprayer()
+    {
+        Quaternion target = Quaternion.Euler(0, 0, rotation);
+        sprayArea.rotation = target;
+
+        // disable spray area if not flying level
+
+        if (22f < rotation && rotation < 338f)
+        {
+            sprayArea.GetComponent<Collider2D>().enabled = false;
+        }
+        else
+        {
+            sprayArea.GetComponent<Collider2D>().enabled = true;
+        }
+    }
+
+    private void ProcessInput()
+    {
+        rotation -= Input.GetAxis("Vertical") * RotationSpeed;
+
+        if (rotation < 0)
+        {
+            rotation += 360;
+        }
+        else if (rotation > 360)
+        {
+            rotation -= 360;
+        }
+    }
+
+    private void UpdateSprite()
+    {
+        int spriteIndex = Mathf.FloorToInt((rotation + 22) / 45);
+        if (spriteIndex >= planeSprites.Length)
+        {
+            spriteIndex = 0;
+        }
+        this.GetComponent<SpriteRenderer>().sprite = planeSprites[spriteIndex];
+    }
+
+    #endregion Private Methods
 }
